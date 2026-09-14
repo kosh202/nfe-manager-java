4b9e5317975f28ab739ade50c243ff272b17839d
Raphael Shodi <80370866+kosh202@users.noreply.github.com>
2026-09-14 00:22:50 -0300

    pegando dados de um xml de exemplo
    primeiros testes

--- src/main/java/br/com/nfe/manager/Main.java
+++ src/main/java/br/com/nfe/manager/Main.java
@@ -1,60 +1,45 @@
 package br.com.nfe.manager;
 
-import br.com.nfe.manager.model.Empresa;
+import br.com.nfe.manager.parser.NFeXmlParser;
 import br.com.nfe.manager.model.ItemNFe;
-import br.com.nfe.manager.model.NFe;
-import br.com.nfe.manager.model.Produto;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
-import java.util.List;
+
+import org.w3c.dom.Document;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        Empresa empresa = new Empresa(
-                "00822602000124",
-                "Plotag Sistemas e Suprimentos Ltda"
-        );
+        NFeXmlParser parser = new NFeXmlParser();
 
-        Produto produto = new Produto(
-                "B17025056",
-                "PAPEL MAXPLOT"
-        );
+        Document documento = parser.carregarXml(
+                "src/main/resources/xml/nfe-exemplo-01.xml");
 
-        ItemNFe item = new ItemNFe(
-                produto,
-                new BigDecimal("1.0000"),
-                new BigDecimal("138.3000"),
-                new BigDecimal("138.30")
-        );
+        String chave = parser.extrairChaveAcesso(documento);
+        String numero = parser.extrairNumero(documento);
+        String cnpj = parser.extrairCnpj(documento);
+        String nomeEmpresa = parser.extrairNomeEmpresa(documento);
+        OffsetDateTime data = parser.extrairData(documento);
+        BigDecimal valorTotal = parser.extrairValorTotal(documento);
 
-        NFe nfe = new NFe(
-                "35150300822602000124550010009923461099234656",
-                "992346",
-                OffsetDateTime.parse("2015-03-27T09:40:00-03:00"),
-                empresa,
-                new BigDecimal("689.91"),
-                List.of(item)
-        );
+        int quantidadeItens = parser.contarItens(documento);
 
-        System.out.println("NF-e: " + nfe.getNumero());
-        System.out.println("Chave: " + nfe.getChaveAcesso());
-        System.out.println("Empresa: " + nfe.getEmpresa().getNome());
-        System.out.println("CNPJ: " + nfe.getEmpresa().getCnpj());
-        System.out.println("Valor total: " + nfe.getValorTotal());
+        ItemNFe item = parser.extrairPrimeiroItem(documento);
 
-        System.out.println("\nItens:");
+        System.out.println("Código: " + item.getProduto().getCodigo());
+        System.out.println("Nome: " + item.getProduto().getNome());
+        System.out.println("Quantidade: " + item.getQuantidade());
+        System.out.println("Preço unitário: " + item.getPrecoUnitario());
+        System.out.println("Valor total: " + item.getValorTotal());
 
-        for (ItemNFe itemNFe : nfe.getItens()) {
-            System.out.println(
-                    itemNFe.getProduto().getNome()
-                    + " - "
-                    + itemNFe.getQuantidade()
-                    + " x "
-                    + itemNFe.getPrecoUnitario()
-            );
-        }
+        System.out.println("Chave de acesso: " + chave);
+        System.out.println("Número da NF-e: " + numero);
+        System.out.println("CNPJ: " + cnpj);
+        System.out.println("Empresa: " + nomeEmpresa);
+        System.out.println("Data: " + data);
+        System.out.println("Valor total: " + valorTotal);
+        System.out.println("Quantidade de itens: " + quantidadeItens);
     }
 }
---
+++ src/main/java/br/com/nfe/manager/parser/NFeXmlParser.java
@@ -0,0 +1,149 @@
+package br.com.nfe.manager.parser;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import br.com.nfe.manager.model.ItemNFe;
+import br.com.nfe.manager.model.Produto;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class NFeXmlParser {
+
+    public Document carregarXml(String caminho) throws Exception {
+
+        File arquivo = new File(caminho);
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        return builder.parse(arquivo);
+    }
+
+    public String extrairChaveAcesso(Document documento) {
+
+        Element infNFe = (Element) documento
+                .getElementsByTagName("infNFe")
+                .item(0);
+
+        String id = infNFe.getAttribute("Id");
+
+        return id.substring(3);
+    }
+
+    public String extrairNumero(Document documento) {
+
+        Element nNF = (Element) documento
+                .getElementsByTagName("nNF")
+                .item(0);
+
+        return nNF.getTextContent();
+    }
+
+    public String extrairCnpj(Document documento) {
+
+        Element emit = (Element) documento
+                .getElementsByTagName("emit")
+                .item(0);
+
+        Element cnpj = (Element) emit
+                .getElementsByTagName("CNPJ")
+                .item(0);
+
+        return cnpj.getTextContent();
+    }
+
+    public String extrairNomeEmpresa(Document documento) {
+
+        Element emit = (Element) documento
+                .getElementsByTagName("emit")
+                .item(0);
+
+        Element xNome = (Element) emit
+                .getElementsByTagName("xNome")
+                .item(0);
+
+        return xNome.getTextContent();
+    }
+
+    public OffsetDateTime extrairData(Document documento) {
+
+        Element dhEmi = (Element) documento
+                .getElementsByTagName("dhEmi")
+                .item(0);
+
+        return OffsetDateTime.parse(dhEmi.getTextContent());
+    }
+
+    public BigDecimal extrairValorTotal(Document documento) {
+
+        Element vNF = (Element) documento
+                .getElementsByTagName("vNF")
+                .item(0);
+
+        return new BigDecimal(vNF.getTextContent());
+    }
+
+    public int contarItens(Document documento) {
+
+        return documento
+                .getElementsByTagName("det")
+                .getLength();
+    }
+
+    public ItemNFe extrairPrimeiroItem(Document documento) {
+
+        NodeList itens = documento.getElementsByTagName("det");
+
+        Element primeiroItem = (Element) itens.item(0);
+
+        Element produtoElement = (Element) primeiroItem
+                .getElementsByTagName("prod")
+                .item(0);
+
+        Element codigo = (Element) produtoElement
+                .getElementsByTagName("cProd")
+                .item(0);
+
+        Element nome = (Element) produtoElement
+                .getElementsByTagName("xProd")
+                .item(0);
+
+        Element quantidade = (Element) produtoElement
+                .getElementsByTagName("qCom")
+                .item(0);
+
+        Element precoUnitario = (Element) produtoElement
+                .getElementsByTagName("vUnCom")
+                .item(0);
+
+        Element valorTotal = (Element) produtoElement
+                .getElementsByTagName("vProd")
+                .item(0);
+
+        Produto produto = new Produto(
+                codigo.getTextContent(),
+                nome.getTextContent());
+
+        BigDecimal quantidadeValor = new BigDecimal(
+                quantidade.getTextContent());
+
+        BigDecimal precoUnitarioValor = new BigDecimal(
+                precoUnitario.getTextContent());
+
+        BigDecimal valorTotalValor = new BigDecimal(
+                valorTotal.getTextContent());
+
+        return new ItemNFe(
+                produto,
+                quantidadeValor,
+                precoUnitarioValor,
+                valorTotalValor);
+    }
+}
